00f7800fd5398a7f30fa86a8ddf5b57c6e62e171
felixfavour <felixfavour0@gmail.com>
2024-08-17 14:58:55 +0100

    fix: issues + SEO desc+title

--- components/layouts/AuthLayout.js
+++ components/layouts/AuthLayout.js
@@ -2,6 +2,7 @@
 import { usePathname, useSearchParams } from "next/navigation"
 import { useRouter } from "next/navigation"
 import Logo from "@/components/Logo.js"
+import { Suspense } from "react"
 
 export default function AuthLayout({ children }) {
   const pathname = usePathname()
@@ -9,12 +10,14 @@ export default function AuthLayout({ children }) {
   const router = useRouter()
 
   return (
-    <div className="auth-bg min-h-[100vh] min-w-[100vw] bg-gradient-to-b from-white to-[#6176AE80]">
-      <div className="auth-header w-full flex flex-col items-center justify-center h-[30vh]">
-        <Logo />
-        <h1 className="font-bold text-2xl text-gray-600 mt-4">Learnmap</h1>
+    <Suspense>
+      <div className="auth-bg min-h-[100vh] min-w-[100vw] bg-gradient-to-b from-white to-[#6176AE80]">
+        <div className="auth-header w-full flex flex-col items-center justify-center h-[30vh]">
+          <Logo />
+          <h1 className="font-bold text-2xl text-gray-600 mt-4">Learnmap</h1>
+        </div>
+        <main>{children}</main>
       </div>
-      <main>{children}</main>
-    </div>
+    </Suspense>
   )
 }
